448c8a4dfde7d86b907d3990ea4e17a0d9c66412
nicocorradini <nicocorradini471@gmail.com>
2024-04-13 14:57:34 -0300

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'local',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
