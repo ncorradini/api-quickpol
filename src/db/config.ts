8628f03e7c99d77bce88fb5d822a6267efbfd57c
nicocorradini <nicocorradini471@gmail.com>
2024-04-13 14:58:30 -0300

    feat: add docker config and connect database

---
+++ src/db/config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_USER_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+  autoLoadEntities: true,
+  synchronize: false,
+};
